83b3eaa6160637115e1f3b2e9425c766baa4a336
Oleksii Talakh <oleksii.talakh@gmail.com>
2021-04-21 13:24:49 +0300

    Add getters for project fields

--- google-cloud-bigquery/src/main/java/com/google/cloud/bigquery/Project.java
+++ google-cloud-bigquery/src/main/java/com/google/cloud/bigquery/Project.java
@@ -3,12 +3,20 @@ package com.google.cloud.bigquery;
 import com.google.common.base.MoreObjects;
 
 public class Project {
-    private String id;
-    private String friendlyName;
+    private final String id;
+    private final String friendlyName;
 
     public Project(String id, String friendlyName) {
         this.id = id;
         this.friendlyName = friendlyName;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getFriendlyName() {
+        return friendlyName;
     }
 
     @Override
